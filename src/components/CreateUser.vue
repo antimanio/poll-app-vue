8b32cf97a99da6f7199c09d4da76d48eb5bbfecc
Anton Tran <anton.tran33@gmail.com>
2025-09-12 16:45:04 +0200

    start with adding one componenet and axios for http

---
+++ src/components/CreateUser.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Create User</h2>
+    <input v-model="username" placeholder="Enter username" />
+    <button @click="createUser">Create</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      message: ''
+    };
+  },
+  methods: {
+    async createUser() {
+      try {
+        const response = await axios.post('http://localhost:5000/users', { username: this.username });
+        this.message = `User ${response.data.username} created!`;
+      } catch (err) {
+        this.message = 'Error creating user';
+      }
+    }
+  }
+};
+</script>
