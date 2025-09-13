0cceedef98eabd5c3c1bda30896627851b6cd4fa
Anton Tran <anton.tran33@gmail.com>
2025-09-13 12:31:13 +0200

    configure primevue

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -1,31 +1,51 @@
 <template>
   <div>
-    <h2>Create User</h2>
-    <input v-model="username" placeholder="Enter username" />
-    <button @click="createUser">Create</button>
+    <!-- Create button -->
+    <Button label="Create User" icon="pi pi-check" @click="createUser" />
+
+    <!-- Load users button -->
+    <Button label="Load Users" icon="pi pi-users" @click="getUsers" class="ml-2" />
+
+    <!-- Show feedback message -->
     <p>{{ message }}</p>
+
+    <!-- Show list of users -->
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.username }} - {{ user.email }}
+      </li>
+    </ul>
   </div>
 </template>
 
-<script>
-import axios from 'axios';
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
 
-export default {
-  data() {
-    return {
-      username: '',
-      message: ''
-    };
-  },
-  methods: {
-    async createUser() {
-      try {
-        const response = await axios.post('http://localhost:5000/users', { username: this.username });
-        this.message = `User ${response.data.username} created!`;
-      } catch (err) {
-        this.message = 'Error creating user';
-      }
-    }
+// state variables
+const username = ref('')
+const message = ref('')
+const users = ref([])
+
+// create user
+const createUser = async () => {
+  try {
+    const response = await axios.post('http://localhost:8080/api/users', { username: username.value, email: "anton@hvl.no"  })
+    message.value = `User ${response.data.username} created!`
+  } catch (err) {
+    message.value = 'Error creating user'
   }
-};
+}
+
+// fetch all users
+const getUsers = async () => {
+  try {
+    const response = await axios.get('http://localhost:8080/api/users')
+    users.value = response.data
+    message.value = 'Users loaded!'
+  } catch (err) {
+    message.value = 'Error fetching users'
+  }
+}
+
 </script>
